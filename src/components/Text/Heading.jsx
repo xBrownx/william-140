429f7082e2b662a05126ac23699b2f9a1b039dee
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-14 13:48:37 +1000

    Create Heading.jsx

---
+++ src/components/Text/Heading.jsx
@@ -0,0 +1,14 @@
+import styled, {css} from "styled-components";
+
+const fontPrimary = css`
+  font-family: '', serif;
+`
+const fontSecondary = css``
+const fontColourPrimary = css``
+const fontColourSecondary = css``
+const fontColourVariant = css``
+
+const Heading = styled.h1`
+  ${fontPrimary}
+  ${fontColourPrimary}
+`
