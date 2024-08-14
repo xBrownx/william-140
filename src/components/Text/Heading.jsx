9552d82e26b216b975fdb3ba847400bbcaa3d0ac
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-14 16:21:06 +1000

    Update Heading.jsx

--- src/components/Text/Heading.jsx
+++ src/components/Text/Heading.jsx
@@ -3,12 +3,33 @@ import styled, {css} from "styled-components";
 const fontPrimary = css`
   font-family: '', serif;
 `
-const fontSecondary = css``
-const fontColourPrimary = css``
-const fontColourSecondary = css``
-const fontColourVariant = css``
 
-const Heading = styled.h1`
+const fontSecondary = css`
+  font-family: '', serif;
+`
+
+const colourPrimary = css`
+  color: #fff
+`
+
+const colourSecondary = css`
+  color: #fff
+`
+
+const colourVariant = css`
+  color: #fff
+`
+
+export const Heading88Px = styled.h1`
   ${fontPrimary}
   ${fontColourPrimary}
+
+  font-size: var(--font-size-88px);
+  
+`
+
+export const Paragraph18px = styled.p`
+  ${fontSecondary}
+  
+  font-size: var(font-size-18px);
 `
