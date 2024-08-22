d3952d515fe7889f5b6edd2d120e5c342da071a9
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-22 11:09:14 +1000

    Update Design.jsx

--- src/components/constants/Design.jsx
+++ src/components/constants/Design.jsx
@@ -1,1 +1,13 @@
 
+export const design = {
+  subheadingTxt: "DESIGN",
+  headingTxt: "A memorable arrival experience",
+  btnTxt: "Start Tour",
+  menuItems: [
+    {title: "MAIN ENTRY", src: ""},
+    {title: "RECEPTION", src: ""},
+    {title: "LOUNGE LOBBY", src: ""},
+    {title: "SERVERY", src: ""},
+    {title: "CAFE LOUNGE", src: ""},
+  ]
+}
