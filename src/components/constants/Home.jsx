3157cbcdf34db50e1b68e1b60105f3cf0781fc1d
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-22 10:50:35 +1000

    Create Home.jsx

---
+++ src/components/constants/Home.jsx
@@ -0,0 +1,8 @@
+
+export const home = {
+  headingText: "SPACES FOR OUR TENANTS TO THRIVE",
+  paragraphText: "Presenting a range of fully fitted tenancies available to lease. " +
+    "These opportunities, offer stunning views and natural light with " +
+    "access to the building’s first class facilities and amenities.",
+  buttonText: "Discovery Availability"
+};
