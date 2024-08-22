b4e09d5452f3c67821c267fac31f45908c116b82
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-22 11:12:39 +1000

    Update History.jsx

--- src/components/constants/History.jsx
+++ src/components/constants/History.jsx
@@ -1,1 +1,7 @@
-
+export const history = {
+  subheadingTxt: "HISTORY",
+  headingTxt: "The Legacy of 140 William Street",
+  paragraphTxt: "Ideally located in the heart of Melbourne CBD," +
+                "enjoying a prime position on the corner of Bourke street" +
+                "and William Street."
+}
