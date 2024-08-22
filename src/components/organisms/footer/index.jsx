24bc63aea542f844fbd01e24f0e3be58c8033511
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-22 15:07:44 +1000

    Update index.jsx

--- src/components/organisms/footer/index.jsx
+++ src/components/organisms/footer/index.jsx
@@ -3,7 +3,9 @@ import * as S from './styles'
 export const Footer = props => {
   return(
     <S.Wrapper>
-    
+      <div className={"flex left"}></div>
+      <div className={"flex centre"}></div>
+      <div className={"flex right"}></div>
     </S.Wrapper>
   );
 }
