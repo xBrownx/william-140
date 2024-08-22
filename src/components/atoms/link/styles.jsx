c7ddc34540a75a43354efbc9c917dcc021561cb1
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-22 11:29:06 +1000

    Update styles.jsx

--- src/components/atoms/link/styles.jsx
+++ src/components/atoms/link/styles.jsx
@@ -1,3 +1,25 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import {
+    Style as S,
+    Font as F,
+    Colour as C,
+    Media as M
+} from "../globalStyles/";
 
-export const StyledLink = styled.a``
+export const StyledLink = styled.a`
+   transition: all 200ms ease-in-out;
+   ${F.size._18px};
+   ${F.family.secondaryRegular};
+   ${S.margin.none};
+   ${S.padding.none};
+   ${C.primary.neutral};
+   font-weight: 400;
+
+   &:hover {
+        color: black;
+        cursor: pointer;
+   }
+
+  
+   
+`
