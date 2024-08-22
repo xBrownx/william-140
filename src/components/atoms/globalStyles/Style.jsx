b796a2994444362cfebe2dd0f3b4bbe5475e6edf
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-22 12:51:06 +1000

    Update Style.jsx

--- src/components/atoms/globalStyles/Style.jsx
+++ src/components/atoms/globalStyles/Style.jsx
@@ -1,6 +1,45 @@
 import {css} from 'styled-components';
 
+const DESIGN ={width: 1440, height: 856};
+
+const pxToVw = (pxWidth) => 
+    return (pxWidth / DESIGN.width) * 100;
+}
+
+const pxToVh = (pxHeight) => 
+    return (pxHeight / DESIGN.height) * 100;
+}
+
 export const Style = {}
+
+const px = {
+    width: {
+        _8: 0.556,
+        _16:,
+        _32:,
+        _48:,
+        _160:,
+    },
+    height: {
+        _8:,
+        _16:,
+        _24:,
+        _32:,
+        _40:,
+        _48:,
+        _56:,
+        _64:,
+        _72:,
+        _80:,
+        _88:,
+        _96:,
+        _104:,
+        _112:,
+        _120:,
+        _128:,
+        _136:,
+    },
+}
 
 Style.borderRadius = css`
     border-radius: 0.278vw;
@@ -8,34 +47,43 @@ Style.borderRadius = css`
 
 Style.padding = {
     none: css`padding: 0;`,
-    top: {},
-    bottom: {},
-    left: {},
-    right: {},
+    top: {
+        
+    },
+    bottom: {
+        
+    },
+    left: {
+        
+    },
+    right: {
+        
+    },
     x: {
+        __8px: css`padding-inline: ${pxToVw(8)}vw}`,
         _8px: css`padding-inline: 0.556vw;`,
-        _16px: css``,
-        _32px: css``,
-        _48px: css``,
-        _160px: css``,
+        _16px: css`padding-inline: 1.111vw;`,
+        _32px: css`padding-inline: 2.222vw;`,
+        _48px: css`padding-inline: 3.333vw;`,
+        _160px: css`padding-inline: 11.111vw;`,
     },
     y: {
-        _8px: css``,
-        _16px: css``,
-        _26px: css``,
-        _23px: css``,
-        _38px: css``,
-        _41px: css``,
-        _47px: css``,
-        _55px: css``,
-        _72px: css``,
-        _90px: css``,
-        _100px: css``,
-        _109px: css``,
-        _140px: css``,
-        _190px: css``,
-        _280px: css``,
-        _500px: css``,
+        _8px: css`padding-block: 0.889vh;`,
+        _16px: css`padding-block: 1.975vh;`,
+        _26px: css`padding-block: 3.210vh;`,
+        _32px: css`padding-block: 3.951vh;`,
+        _38px: css`padding-block: 4.691vh;`,
+        _41px: css`padding-block: 5.062vh;`,
+        _47px: css`padding-block: 5.802vh;`,
+        _55px: css`padding-block: 6.790vh;`,
+        _72px: css`padding-block: 8.889vh;`,
+        _90px: css`padding-block: 11.111vh;`,
+        _100px: css`padding-block: 12.346vh;`,
+        _109px: css`padding-block: 13.457vh`,
+        _140px: css`padding-block: 17.284vh`,
+        _190px: css`padding-block: 23.457vh`,
+        _280px: css`padding-block: 34.568vh`,
+        _500px: css`padding-block: 61.728vh`,
     },
 
 }
@@ -78,11 +126,4 @@ Style.fullscreen = css`
     height: 100vh;
 `
 
-Style.width = {
-    _8px: '0.556vw',
-}
-
-Style.height = {
-
-}
 
