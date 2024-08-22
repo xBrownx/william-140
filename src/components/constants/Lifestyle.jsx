41c96697d19059d42efe947b50c33ef3c0ea741a
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-22 10:53:34 +1000

    Create Lifestyle.jsx

---
+++ src/components/constants/Lifestyle.jsx
@@ -0,0 +1,24 @@
+import img1 from "../../assets/img/Lifestyle-Carousel-1.png";
+import img2 from "../../assets/img/Lifestyle-Carousel-2.png";
+import img3 from "../../assets/img/Lifestyle-Carousel-3.png";
+import img4 from "../../assets/img/Lifestyle-Carousel-4.png";
+import img5 from "../../assets/img/Lifestyle-Carousel-5.png";
+import img6 from "../../assets/img/Lifestyle-Carousel-6.png";
+import img7 from "../../assets/img/Lifestyle-Carousel-7.png";
+import img8 from "../../assets/img/Lifestyle-Carousel-8.png";
+
+export const lifestyle = {
+  carouselImgs: [
+            {src: img1},
+            {src: img2},
+            {src: img3},
+            {src: img4},
+            {src: img5},
+            {src: img6},
+            {src: img7},
+            {src: img8},
+        ],
+  subheadingTxt: "LIFESTYLE",
+  headingTxt: "Everyday is an experience",
+  paragraphTxt: The essence of Melbourne’s vibrant lifestyle, offering a perfect blend of convenience, culture, and connectivity.
+};
