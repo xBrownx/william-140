57b3ae3e16659c1d033bc06d7ea21d4d8cde033a
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-22 11:52:02 +1000

    Create styles.jsx

---
+++ src/components/molecules/indentedParagraph/styles.jsx
@@ -0,0 +1,8 @@
+import styled, { css } from 'styled-components';
+import { Style as S } from '../atoms';
+
+export const Wrapper = styled.div`
+  border-left: 1px solid var(--color-primary-4);
+  margin: var(--height-38px) 0 0 0;
+  padding: 0 0 var(--height-32px) var(--width-32px);
+`
