b0cc6335ff2cc8d146ca99fcb171b961b7ba609b
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-08-29 14:02:00 +1000

    Create styles.jsx

---
+++ src/components/molecules/parallaxText/styles.jsx
@@ -0,0 +1,12 @@
+import styled, { css } from 'styled-components';
+import { motion } from "framer-motion";
+
+export const Wrapper = styled(motion.div)`
+    whiteSpace: pre-line; 
+    overflow: hidden;
+`
+
+export const StyledSpan = styled(motion.span)`
+    display: inline-block;
+    overflow: hidden;
+`
