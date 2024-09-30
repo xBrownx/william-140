ee77a760fc9183e5f3131301c1921e309d7ab59d
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-30 10:22:43 +1000

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,24 @@
 import './App.css';
 import React, { useRef, lazy, Suspense, useTransition, useEffect, useState } from "react";
 import { imageAssets, videoAssets } from '../../assets/assetLinks';
-import { Header } from "../organisms";
-import Landing from '../pages/landing'
+import { Header, Footer, ScrollingText } from "../organisms";
+
 import ErrorBoundary from "../ErrorBoundary";
-import { ScrollingText } from "../organisms";
 import LoadingScreen from "../organisms/loadingScreen";
 
-const Agency = lazy(() => import('../pages/agency'));
-const Amenities = lazy(() => import('../pages/amenities'));
-const Availability = lazy(() => import('../pages/availabality'));
-const Design = lazy(() => import('../pages/design'));
-const Enquire = lazy(() => import('../pages/enquire'));
-const Gallery = lazy(() => import('../pages/gallery'));
-const History = lazy(() => import('../pages/history'));
-const HeroOne = lazy(() => import('../pages/heroOne'));
-const HeroTwo = lazy(() => import('../pages/heroTwo'));
-const HeroThree = lazy(() => import('../pages/heroThree'));
-const Home = lazy(() => import('../pages/home'));
-const Lifestyle = lazy(() => import('../pages/lifestyle'));
-const Location = lazy(() => import('../pages/location'));
-const Footer = lazy(() => import('../organisms/').then(m => ({ default: m.Footer })));
+import Agency from '../pages/agency';
+import Amenities from '../pages/amenities';
+import Availability from '../pages/availabality';
+import Design from '../pages/design';
+import Enquire from '../pages/enquire';
+import Gallery from '../pages/gallery';
+import History from '../pages/history';
+import HeroOne from '../pages/heroOne';
+import HeroTwo from '../pages/heroTwo';
+import HeroThree from '../pages/heroThree';
+import Home from '../pages/home';
+import Lifestyle from '../pages/lifestyle';
+import Location from '../pages/location';
 
 function App() {
     const [isLoading, setIsLoading] = useState(true);
@@ -79,50 +77,21 @@ function App() {
                 {/*<LoadingScreen />*/}
                 <Header pageRefs={pageRefs} scrollTo={scrollTo} />
                 <Landing ref={pageRefs.landing} scrollTo={() => scrollTo(pageRefs.home)} />
-
-                <Suspense fallback={<div>Loading...</div>}>
                     <Home ref={pageRefs.home} onDiscoverClick={() => scrollTo(pageRefs.availability)} />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <HeroOne />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <Lifestyle />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <Location ref={pageRefs.location} />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <HeroTwo />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <Design ref={pageRefs.design} />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <Amenities ref={pageRefs.amenities} />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <Availability ref={pageRefs.availability} />
-                </Suspense>
                     <ScrollingText />
-                <Suspense fallback={<div>Loading...</div>}>
                     <History ref={pageRefs.history} />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <Gallery />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <Agency ref={pageRefs.agency} />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <HeroThree />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <Enquire ref={pageRefs.enquire} scrollToTop={() => scrollTo(pageRefs.landing)} />
-                </Suspense>
-                <Suspense fallback={<div>Loading...</div>}>
                     <Footer />
-                </Suspense>
             </div>
         </ErrorBoundary>
     );
